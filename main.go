4e4bf5980415fff884457eae86a228941f082f32
Pierre Balzack <96387156+balzack@users.noreply.github.com>
2024-01-09 00:25:18 -0800

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
   if len(args) == 3 {
     port := ":" + args[1]
     store.SetPath(args[2])
-    router := app.NewRouter("/opt/databag/web")
+    router := app.NewRouter("/opt/databag/web/build")
     origins := handlers.AllowedOrigins([]string{"*"})
     methods := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"})
     log.Fatal(http.ListenAndServe(port, handlers.CORS(origins, methods)(router)))
